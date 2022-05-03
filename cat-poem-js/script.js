b7e06aa1b77dd90c94587ccb808ac3e209b1bdfd
lneoa <leonacheun@gmail.com>
2022-05-03 17:13:28 -0400

    edits to cat poem again

--- cat-poem-js/script.js
+++ cat-poem-js/script.js
@@ -6,33 +6,60 @@ what = "c-a-t"
 mode1 = "(mo)"
 mode2 = "b,i;l:e"
 motion = "Fall"
-bigAction = "leAps!"
+bigAction1 = "leA"
+bigAction2 = "ps!"
 action1 = "flOat"
 action2 = "tumblIsh"
 action3 = "drIfts"
-how = "whirlF(Ul)(lY)"
+how1 = "whirlF"
+how2 = "(Ul)(lY)"
 where = "away"
 go = "wanders"
-manner = "exactly"
-thing = "nothing"
+manner1 = "exact"
+manner2 = "ly"
+thing1 = "not"
+thing2 = "hing"
 when = "ever"
-past = "happeneD"
+past1 = "happ"
+past2 = "ene"
+past3 = "D"
 
 
 document.getElementById("demo").innerHTML
 = who + what + mode1
-+ mode2
-+ motion
-+ bigAction
-+ action1
 + "<br>"
++ mode2
++ "<br>"
++ "<br>"
++ motion
++ bigAction1
++ bigAction2
++ action1
 + action2
++ "<br>"
++ "<br>"
 + action3
-+ how 
++ how1
++ "<br>"
++ how2
++ "<br>"
 + "&amp;&amp;&amp;"
-+ go + "&ensp;" + where + ":" + manner
-+ "as if" + "<br>"
-+ thing + " had," + when
-+ past
++ "<br>"
++ "<br>"
++ go + "&ensp;" + where + ":" + manner1
++ "<br>"
++ manner2 + ";"
++ "as if" 
++ "<br>"
++ thing1
++ "<br>"
++ thing2
++ " had," + when
++ past1
++ "<br>"
++ past2
++ "<br>"
++ "<br>"
++ past3
 ;
 
